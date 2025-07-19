fc07552af34dc45b85bb0efb7f9faa2d5a2456a3
TechRemix <shahinlabs@gmail.com>
2025-07-19 15:03:53 +0100

    Update update-item.php

--- php/update-item.php
+++ php/update-item.php
@@ -8,24 +8,24 @@ if (!$conn) {
   exit;
 }
 
-$id       = $_POST['id'];
-$name     = $_POST['name'];
-$details  = $_POST['details'];
-$category = $_POST['category'];
-$quantity = $_POST['quantity'];
-$isbn     = $_POST['isbn'];
-$year     = $_POST['year'];
-$location = $_POST['location'];
-$status   = $_POST['status'];
+$id         = $_POST['id'];
+$name       = $_POST['name'];
+$details    = $_POST['details'];
+$category   = $_POST['category'];
+$quantity   = $_POST['quantity'];
+$item_code  = $_POST['item_code'];
+$year       = $_POST['year'];
+$location   = $_POST['location'];
+$status     = $_POST['status'];
 
 $query = "
   UPDATE items 
-  SET name = $1, details = $2, category = $3, quantity = $4, isbn = $5, year = $6, location = $7, status = $8
+  SET name = $1, details = $2, category = $3, quantity = $4, item_code = $5, year = $6, location = $7, status = $8
   WHERE id = $9
 ";
 
 $result = pg_query_params($conn, $query, [
-  $name, $details, $category, $quantity, $isbn, $year, $location, $status, $id
+  $name, $details, $category, $quantity, $item_code, $year, $location, $status, $id
 ]);
 
 if ($result) {
