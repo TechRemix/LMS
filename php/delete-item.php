ac0296db4a212a17a77b49bc176f7bc66ffe08bd
TechRemix <shahinlabs@gmail.com>
2025-07-26 19:44:13 +0100

    Update delete-item.php

--- php/delete-item.php
+++ php/delete-item.php
@@ -1,7 +1,8 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   // Connect to PostgreSQL
-  $conn = pg_connect("host=dpg-d1tafq6mcj7s73d58avg-a dbname=lms_db_3hx1 user=lms_db_3hx1_user password=ImGmDcnvBzwU1ustoexBQSjvywKJmFsx port=5432");
+  pg_connect("host=ep-delicate-frog-abhvcfve-pooler.eu-west-2.aws.neon.tech dbname=neondb user=neondb_owner password=npg_9Cf7QdRgmcPB sslmode=require options='endpoint=ep-delicate-frog-abhvcfve'");
+
 
   if (!$conn) {
     http_response_code(500);
